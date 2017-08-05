0a497f1f47cffb549cf98d55e37850248d6472de
Abhijeet1850 <Abhijeet.Gupta@nagarro.com>
2017-08-05 19:14:35 +0530

    First Commit

---
+++ src/test/java/com/test/automate/WalletHub/facebookLogin/TC001_VerifyLoginWithInvalidCredentials.java
@@ -0,0 +1,23 @@
+package com.test.automate.WalletHub.facebookLogin;
+
+public class TC001_VerifyLoginWithInvalidCredentials {
+
+	public void setUp()
+	{
+		
+	}
+	
+	public void verifyLoginWithInvalidCredentials()
+	{
+		
+		
+	}
+	
+	
+	public void tearDown()
+	{
+		
+		
+	}
+	
+}
